b03386824b67b9841fbced7e6364694bf3481b2b
SeedPuller <SeedPuller@gmail.com>
2020-06-19 17:46:59 +0430

    c++ models added

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QDebug>
+#include "newsmodel.h"
+#include "loginhandler.h"
+//#include <commentmodel.h>
 
 int main(int argc, char *argv[])
 {
+    qmlRegisterType<NewsModel>("API.NewsModel", 0, 1, "NewsModel");
+    qmlRegisterType<LoginHandler>("API.LoginHandler", 0, 1, "LoginHandler");
+//    qmlRegisterType<CommentModel>("API.CommentModel", 0, 1, "CommentModel");
+
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-
     QGuiApplication app(argc, argv);
-
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
